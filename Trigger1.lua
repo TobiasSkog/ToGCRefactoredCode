e81b02ab8c016040df5276fd7a00ee4a27adef78
Tobias Skog <tobias.skog.net23@edu.edugrade.se>
2023-10-06 21:43:01 +0200

    Create Trigger1.lua

---
+++ Trigger1.lua
@@ -0,0 +1,14 @@
+-- GROUP_ROSTER_UPDATE
+function()
+    if (UnitIsGroupAssistant("player") or UnitIsGroupLeader("player")) then
+        aura_env.AoL = true
+    else
+        aura_env.AoL = false
+    end
+    aura_env.AoL = true
+    if aura_env.AoL and not aura_env.pewpew then  
+        aura_env.newAssigns, aura_env.resetCounter = aura_env.getImportedAssignments()
+        DevTool:AddData(aura_env.newAssigns, "aura_env.newAssigns")
+        return true    
+    end
+end
